9590c480c80846a8166e0c32a8a2129dcd31040a
Kush Gupta <Kushg827@gmail.com>
2025-06-14 22:58:51 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -13,29 +13,5 @@ app.listen(3000, async () => {
     await connect();
     console.log('connected to database');
 
-    // const tweet = await Tweet.create({
-    //     content: 'First tweet',
-    //     userEmail: 'a@b.com'
-    // });
-    // console.log(tweet); 
-
-    // const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.get('684d5fecc043799caadac59d');
-    // console.log(tweet);
-
-    // const tweet = await tweetRepo.create({content: 'my tweet'});
-    // console.log(tweet);
-    // tweet.comments.push({content: 'first comment'});
-    // await tweet.save();
-    // console.log(tweet);
-
-    // const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.create({content: 'Tweet with comment schema'})
-    // console.log(tweet);
-    // const comment = await Comment.create({content: 'new comment'});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-
-    // const tweet = await tweetRepo.getWithComments('684d77543ff915f8c041c6aa')
-    // console.log(tweet);
+    
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -3,22 +3,17 @@ const mongoose = require('mongoose');
 const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
-        required: true
+        required: true,
+        max: [250, 'Tweets cannot be longer than 250 characters.']
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
+    hashtags: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
+            ref: 'Hashtag'
         }
     ]
+    
 }, { timestamps: true });
-
-tweetSchema.virtual('contentWithEmail').get(function process() {
-    return `${this.content} \nCreated by: ${this.userEmail}`
-})
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
